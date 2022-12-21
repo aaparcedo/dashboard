98e284343fc1ce663ce862e4823ae973aef988d6
aaparcedo <aaparcedo.io@gmail.com>
2022-12-21 18:43:39 -0500

    add form section

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import Dashboard from "./scenes/dashboard";
 // import Invoices from "./scenes/invoices";
 // import Contacts from "./scenes/Contacts";
 // import Bar from "./scenes/bar";
-// import Form from "./scenes/form";
+import Form from "./scenes/form";
 // import Line from "./scenes/line";
 // import Pie from "./scenes/pie";
 // import FAQ from "./scenes/faq";
@@ -30,7 +30,7 @@ function App() {
               {/* <Route path="/team" element={<Team />} /> */}
               {/* <Route path="/contacts" element={<Contacts />} /> */}
               {/* <Route path="/invoices" element={<Invoices />} /> */}
-              {/* <Route path="/form" element={<Form />} /> */}
+              <Route path="/form" element={<Form />} />
               {/* <Route path="/bar" element={<Bar />} /> */}
               {/* <Route path="/pie" element={<Pie />} /> */}
               {/* <Route path="/line" element={<Line />} /> */}
