920e94be45ecf3e4ebe2a462214cb168ff557fbb
Josue7211 <bobbyparzero@gmail.com>
2022-12-24 17:17:31 -0500

    fix firebase

--- src/firebase.js
+++ src/firebase.js
@@ -10,12 +10,12 @@ import { getAuth } from "firebase/auth";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "process.env.REACT_APP_FIREBASE_KEY",
-  authDomain: "REACT_APP_FIREBASE_DOMAIN",
-  projectId: "REACT_APP_FIREBASE_PROJECT_ID",
-  storageBucket: "REACT_APP_FIREBASE_STORAGE_BUCKET",
-  messagingSenderId: "REACT_APP_FIREBASE_SENDER_ID",
-  appId: "REACT_APP_FIREBASE_APP_ID",
-  measurementId: "REACT_APP_FIREBASE_MEASUREMENT_ID"
+  authDomain: "process.env.REACT_APP_FIREBASE_DOMAIN",
+  projectId: "process.env.REACT_APP_FIREBASE_PROJECT_ID",
+  storageBucket: "process.env.REACT_APP_FIREBASE_STORAGE_BUCKET",
+  messagingSenderId: "process.env.REACT_APP_FIREBASE_SENDER_ID",
+  appId: "process.env.REACT_APP_FIREBASE_APP_ID",
+  measurementId: "process.env.REACT_APP_FIREBASE_MEASUREMENT_ID"
 };
 
 // Initialize Firebase
