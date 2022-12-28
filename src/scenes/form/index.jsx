0f4e89a9eac02121a56b7dd0cb9c638d156affcc
aaparcedo <aaparcedo.io@gmail.com>
2022-12-27 21:29:02 -0500

    add fields to client form

--- src/scenes/form/index.jsx
+++ src/scenes/form/index.jsx
@@ -14,21 +14,50 @@ const initialValues = {
   firstName: "",
   lastName: "",
   email: "",
-  contact: "",
-  address1: "",
-  address2: "",
+  phone: "",
+  streetAddress: "",
+  city: "",
+  zipCode: "",
+  civilStatus: "",
+  alienNumber: "",
+  visaNumber: "",
+  i94Number: "",
+  dateOfArrival: "",
+  immigrationStatusOnArrival: "",
+  currentImmigrationStatus: "",
+  religion: "",
+  weight: "",
+  height: "",
+  eyeColor: "",
+  hairColor: "",
+
 };
 
 const userSchema = yup.object().shape({
   firstName: yup.string().required("required"),
   lastName: yup.string().required("required"),
   email: yup.string().email("Invalid email").required("required"),
-  contact: yup
+  phone: yup
     .string()
     .matches(phoneRegExp, "Phone number is not valid")
     .required("required"),
-  address1: yup.string().required("required"),
-  address2: yup.string().required("required"),
+  // THIS HAS TO BE CHANGED TO A VALID DATE OF BIRTH INSTEAD OF JUST A STRING
+  dateOfBirth: yup.string().required("required"),
+  streetAddress: yup.string().required("required"),
+  city: yup.string().required("required"),
+  zipCode: yup.string().required("required"),
+  civilStatus: yup.string().required("required"),
+  alienNumber: yup.string().required("required"),
+  visaNumber: yup.string().required("required"),
+  i94Number: yup.string().required("required"),
+  dateOfArrival: yup.string().required("required"),
+  immigrationStatusOnArrival: yup.string().required("required"),
+  currentImmigrationStatus: yup.string().required("required"),
+  religion: yup.string().required("required"),
+  weight: yup.string().required("required"),
+  height: yup.string().required("required"),
+  eyeColor: yup.string().required("required"),
+  hairColor: yup.string().required("required"),
 });
 
 const Form = () => {
@@ -107,30 +136,30 @@ const Form = () => {
                   name="email"
                   error={!!touched.email && !!errors.email}
                   helperText={touched.email && errors.email}
-                  sx={{ gridColumn: "span 4" }}
+                  sx={{ gridColumn: "span 2" }}
                 />
                 <TextField
                   fullWidth
                   variant="filled"
                   type="text"
-                  label="Contact Number"
+                  label="Phone Number"
                   onBlur={handleBlur}
                   onChange={handleChange}
-                  value={values.contact}
-                  name="contact"
-                  error={!!touched.contact && !!errors.contact}
-                  helperText={touched.contact && errors.contact}
-                  sx={{ gridColumn: "span 4" }}
+                  value={values.phone}
+                  name="phone"
+                  error={!!touched.phone && !!errors.phone}
+                  helperText={touched.phone && errors.phone}
+                  sx={{ gridColumn: "span 2" }}
                 />
                 <TextField
                   fullWidth
                   variant="filled"
                   type="text"
-                  label="Address 1"
+                  label="Street Address"
                   onBlur={handleBlur}
                   onChange={handleChange}
                   value={values.address1}
-                  name="address1"
+                  name="streetAddress"
                   error={!!touched.address1 && !!errors.address1}
                   helperText={touched.address1 && errors.address1}
                   sx={{ gridColumn: "span 4" }}
@@ -139,15 +168,185 @@ const Form = () => {
                   fullWidth
                   variant="filled"
                   type="text"
-                  label="Address 2"
+                  label="City"
                   onBlur={handleBlur}
                   onChange={handleChange}
-                  value={values.address2}
-                  name="address2"
-                  error={!!touched.address2 && !!errors.address2}
-                  helperText={touched.address2 && errors.address2}
+                  value={values.city}
+                  name="city"
+                  error={!!touched.city && !!errors.city}
+                  helperText={touched.city && errors.city}
+                  sx={{ gridColumn: "span 2" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Zip Code"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.zipCode}
+                  name="zipCode"
+                  error={!!touched.zipCode && !!errors.zipCode}
+                  helperText={touched.zipCode && errors.zipCode}
+                  sx={{ gridColumn: "span 2" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Civil Status"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.civilStatus}
+                  name="civilStatus"
+                  error={!!touched.civilStatus && !!errors.civilStatus}
+                  helperText={touched.civilStatus && errors.civilStatus}
                   sx={{ gridColumn: "span 4" }}
                 />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Alien Number"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.alienNumber}
+                  name="alienNumber"
+                  error={!!touched.alienNumber && !!errors.alienNumber}
+                  helperText={touched.alienNumber && errors.alienNumber}
+                  sx={{ gridColumn: "span 4" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Visa Number"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.visaNumber}
+                  name="visaNumber"
+                  error={!!touched.visaNumber && !!errors.visaNumber}
+                  helperText={touched.visaNumber && errors.visaNumber}
+                  sx={{ gridColumn: "span 4" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="I-94"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.i94Number}
+                  name="i94Number"
+                  error={!!touched.i94Number && !!errors.i94Number}
+                  helperText={touched.i94Number && errors.i94Number}
+                  sx={{ gridColumn: "span 4" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Date of Arrival Into US"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.dateOfArrival}
+                  name="dateOfArrival"
+                  error={!!touched.dateOfArrival && !!errors.dateOfArrival}
+                  helperText={touched.dateOfArrival && errors.dateOfArrival}
+                  sx={{ gridColumn: "span 4" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Immigration Status Upon Arrival"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.immigrationStatusOnArrival}
+                  name="immigrationStatusOnArrival"
+                  error={!!touched.immigrationStatusOnArrival && !!errors.immigrationStatusOnArrival}
+                  helperText={touched.immigrationStatusOnArrival && errors.immigrationStatusOnArrival}
+                  sx={{ gridColumn: "span 4" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Current Immigration Status"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.currentImmigrationStatus}
+                  name="currentImmigrationStatus"
+                  error={!!touched.currentImmigrationStatus && !!errors.currentImmigrationStatus}
+                  helperText={touched.currentImmigrationStatus && errors.currentImmigrationStatus}
+                  sx={{ gridColumn: "span 4" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Religion"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.religion}
+                  name="religion"
+                  error={!!touched.religion && !!errors.religion}
+                  helperText={touched.religion && errors.religion}
+                  sx={{ gridColumn: "span 4" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Weight"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.weight}
+                  name="weight"
+                  error={!!touched.weight && !!errors.weight}
+                  helperText={touched.weight && errors.weight}
+                  sx={{ gridColumn: "span 4" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Height"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.height}
+                  name="height"
+                  error={!!touched.height && !!errors.height}
+                  helperText={touched.height && errors.height}
+                  sx={{ gridColumn: "span 4" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Eye Color"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.eyeColor}
+                  name="eyeColor"
+                  error={!!touched.eyeColor && !!errors.eyeColor}
+                  helperText={touched.eyeColor && errors.eyeColor}
+                  sx={{ gridColumn: "span 4" }}
+                />
+                <TextField
+                  fullWidth
+                  variant="filled"
+                  type="text"
+                  label="Hair Color"
+                  onBlur={handleBlur}
+                  onChange={handleChange}
+                  value={values.hairColor}
+                  name="hairColor"
+                  error={!!touched.hairColor && !!errors.hairColor}
+                  helperText={touched.hairColor && errors.hairColor}
+                  sx={{ gridColumn: "span 4" }}
+                />
+                
               </Box>
               <Box display="flex" justifyContent="end" mt="20px">
                 <Button type="submit" color="secondary" variant="contained">
