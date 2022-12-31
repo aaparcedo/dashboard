30bf5d498785b97278c589b329c7f702c6c0f523
aaparcedo <aaparcedo.io@gmail.com>
2022-12-31 09:36:28 -0500

    fix sidebar client routing

--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -148,8 +148,8 @@ const Sidebar = () => {
               setSelected={setSelected}
             />
             <Item
-              title="Contacts Information"
-              to="/contacts"
+              title="Clients"
+              to="/clients"
               icon={<ContactsOutlinedIcon />}
               selected={selected}
               setSelected={setSelected}
