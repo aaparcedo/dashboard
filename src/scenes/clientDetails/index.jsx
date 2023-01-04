3e9ea5760d11b2e58c6cbe4713ac499a6c1f8007
aaparcedo <aaparcedo.io@gmail.com>
2023-01-04 15:57:48 -0500

    add fields to dataGrid in clientDetails

--- src/scenes/clientDetails/index.jsx
+++ src/scenes/clientDetails/index.jsx
@@ -9,6 +9,7 @@ import { useState, useEffect } from "react";
 import { collection, getDocs, onSnapshot } from "firebase/firestore";
 import { useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom";
+import { spacing } from "@mui/system";
 
 const ClientDetails = () => {
   const [user, loading, error] = useAuthState(auth);
@@ -43,62 +44,83 @@ const ClientDetails = () => {
     return unsubscribe; // This function will be called when the component unmounts to stop listening to the snapshot
   }, []);
 
-  useEffect(() => {
-    if (!user) {
-      navigate("/");
-    }
-  }, [user, navigate]);
+  // useEffect(() => {
+  //   if (!user) {
+  //     navigate("/");
+  //   }
+  // }, [user, navigate]);
 
   let client;
-
-  // console.log(client);
 
   if (clients[clientId]) {
     client = clients[clientId];
   }
-  // console.log(client);
 
   let columns = [];
   let rows = [];
 
   if (client) {
     columns = [
-      { field: "id", headerName: "Key", width: 150 },
-      { field: "value", headerName: "Value", width: 130 },
+      { field: "id", headerName: "Key", width: 250 },
+      { field: "value", headerName: "Value", width: 250 },
     ];
 
     rows = [
       { id: "First Name", value: client.firstName },
       { id: "Last Name", value: client.lastName },
+      { id: "Email", value: client.email },
+      { id: "Phone Number", value: client.phone },
+      { id: "Street Address", value: client.streetAddress },
+      { id: "City", value: client.city },
+      { id: "Zip Code", value: client.zipCode },
+      { id: "Civil Status", value: client.civilStatus },
+      { id: "Alien Number", value: client.alienNumber },
+      { id: "Visa Number", value: client.visaNumber },
+      { id: "I-94", value: client.i94Number },
+      { id: "Date of Arrival Into the US", value: client.dateOfArrival },
+      {
+        id: "Current Immigration Status",
+        value: client.currentImmigrationStatus,
+      },
+      { id: "Religion", value: client.religion },
+      { id: "Weight", value: client.weight },
+      { id: "Height", value: client.height },
+      { id: "Eye Color", value: client.eyeColor },
+      { id: "Hair Color", value: client.hairColor },
     ];
   }
 
-  console.log(rows);
-  // console.log(columns);
 
   return (
     client && (
-      // <Box
-      //   display="flex"
-      //   flexDirection="column"
-      //   justifyContent="center"
-      //   alignItems="center"
-      // >
-      //   {/* <Header title="CLIENT DETAILS" subtitle="Client details page" /> */}
-      //   <Box>
-      //     <Typography>First Name: {client.firstName}</Typography>
-      //     <Typography>Last Name: {client.lastName}</Typography>
-      //     <Typography>Email: {client.email}</Typography>
-      //   </Box>
-      //   {/* <Box bgcolor="gray" width= '25%'>
-      //     <Typography>here</Typography>
-      //   </Box> */}
-      // </Box>
-      <div>
-        <Box sx={{ height: 400, width: "50%" }}>
-          <DataGrid rows={rows} columns={columns} pageSize={5} />
+      <Box
+        m="20px"
+        sx={{
+          justifyContent: "center",
+          height: "100%",
+          mx: "auto",
+          my: "auto",
+        }}
+      >
+        <Box
+          sx={{
+            display: "flex",
+            height: "100%",
+            width: 600,
+            justifyContent: "center",
+            alignItems: "bottom",
+            mx: "auto",
+            my: "auto"
+          }}
+        >
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            sx={{ justifyContent: "center" }}
+            pageSize={18}
+          />
         </Box>
-      </div>
+      </Box>
     )
   );
 };
