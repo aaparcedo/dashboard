beed3eef085ca67f83fd2d0142af04886b83025e
aaparcedo <aaparcedo.io@gmail.com>
2023-01-16 11:48:58 -0500

    show client count on dashboard

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -5,6 +5,7 @@ import DownloadOutlinedIcon from "@mui/icons-material/DownloadOutlined";
 import EmailIcon from "@mui/icons-material/Email";
 import PointOfSaleIcon from "@mui/icons-material/PointOfSale";
 import PersonAddIcon from "@mui/icons-material/PersonAdd";
+import Person from "@mui/icons-material/Person";
 import TrafficIcon from "@mui/icons-material/Traffic";
 import Header from "../../components/Header";
 import LineChart from "../../components/LineChart";
@@ -13,55 +14,75 @@ import BarChart from "../../components/BarChart";
 import StatBox from "../../components/StatBox";
 import ProgressCircle from "../../components/ProgressCircle";
 import { useNavigate } from "react-router-dom";
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "../../firebase";
+import { auth, db } from "../../firebase";
+
+import { collection, getCountFromServer, onSnapshot } from "firebase/firestore";
 
 const Dashboard = () => {
-  const [user, loading, error] = useAuthState(auth);
+  const [clientCount, setClientCount] = useState(0);
+  const clientsCollectionRef = collection(db, "clients");
+  let snapshot;
+  const getClientCount = async () => {
+    snapshot = await getCountFromServer(clientsCollectionRef);
+    setClientCount(snapshot.data().count);
+    
+    return snapshot;
+  };
+  useEffect(() => {
+    getClientCount();
+  })
+
+  const [
+    user,
+    // loading,
+    //  error
+  ] = useAuthState(auth);
 
   const theme = useTheme();
   const colors = tokens(theme.palette.mode);
-  const navigate  = useNavigate();
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (!user) {
-      navigate('/');;
+      navigate("/");
     }
   }, [user, navigate]);
 
-  return (user && (
-    <Box m="20px">
-      {/* HEADER */}
-      <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
+  return (
+    user && (
+      <Box m="20px">
+        {/* HEADER */}
+        <Box display="flex" justifyContent="space-between" alignItems="center">
+          <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
 
-        <Box>
-          <Button
-            sx={{
-              backgroundColor: colors.blueAccent[700],
-              color: colors.grey[100],
-              fontSize: "14px",
-              fontWeight: "bold",
-              padding: "10px 20px",
-            }}
-          >
-            <DownloadOutlinedIcon sx={{ mr: "10px" }} />
-            Download Reports
-          </Button>
+          <Box>
+            <Button
+              sx={{
+                backgroundColor: colors.blueAccent[700],
+                color: colors.grey[100],
+                fontSize: "14px",
+                fontWeight: "bold",
+                padding: "10px 20px",
+              }}
+            >
+              <DownloadOutlinedIcon sx={{ mr: "10px" }} />
+              Download Reports
+            </Button>
+          </Box>
         </Box>
-      </Box>
 
-      {/* GRID & CHARTS */}
-      <Box
-        display="grid"
-        gridTemplateColumns="repeat(12, 1fr)"
-        gridAutoRows="140px"
-        gap="20px"
-      >
-        {/* ROW 1 */}
-        {/* EMAIL SENT BOX */}
-        {/* <Box
+        {/* GRID & CHARTS */}
+        <Box
+          display="grid"
+          gridTemplateColumns="repeat(12, 1fr)"
+          gridAutoRows="140px"
+          gap="20px"
+        >
+          {/* ROW 1 */}
+          {/* EMAIL SENT BOX */}
+          {/* <Box
           gridColumn="span 3"
           backgroundColor={colors.primary[400]}
           display="flex"
@@ -80,8 +101,8 @@ const Dashboard = () => {
             }
           />
         </Box> */}
-        {/* SALES BOX */}
-        {/* <Box
+          {/* SALES BOX */}
+          {/* <Box
           gridColumn="span 3"
           backgroundColor={colors.primary[400]}
           display="flex"
@@ -100,29 +121,29 @@ const Dashboard = () => {
             }
           />
         </Box> */}
-        {/* TOTAL CLIENTS BOX
+          {/* TOTAL CLIENTS BOX
         CONNECT TO FIRESTORE */}
-        <Box
-          gridColumn="span 3"
-          backgroundColor={colors.primary[400]}
-          display="flex"
-          alignItems="center"
-          justifyContent="center"
-        >
-          <StatBox
-            title="32,441"
-            subtitle="Clients"
-            progress="0.30"
-            increase="+5%"
-            icon={
-              <PersonAddIcon
-                sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
-              />
-            }
-          />
-        </Box>
-        {/* TRAFFIC RECEIVED BOX */}
-        {/* <Box
+          <Box
+            gridColumn="span 3"
+            backgroundColor={colors.primary[400]}
+            display="flex"
+            alignItems="center"
+            justifyContent="center"
+          >
+            <StatBox
+              title={clientCount}
+              subtitle="Clients"
+              // progress="0.30"
+              // increase="+5%"
+              icon={
+                <Person
+                  sx={{ color: colors.greenAccent[600], fontSize: "26px" }}
+                />
+              }
+            />
+          </Box>
+          {/* TRAFFIC RECEIVED BOX */}
+          {/* <Box
           gridColumn="span 3"
           backgroundColor={colors.primary[400]}
           display="flex"
@@ -142,9 +163,9 @@ const Dashboard = () => {
           />
         </Box> */}
 
-        {/* ROW 2 */}
-        {/* REVENUE GENERATED CHART */}
-        {/* <Box
+          {/* ROW 2 */}
+          {/* REVENUE GENERATED CHART */}
+          {/* <Box
           gridColumn="span 8"
           gridRow="span 2"
           backgroundColor={colors.primary[400]}
@@ -186,8 +207,8 @@ const Dashboard = () => {
         </Box>
          */}
 
-        {/* RECENT TRANSACTIONS */}
-        {/* <Box
+          {/* RECENT TRANSACTIONS */}
+          {/* <Box
           gridColumn="span 4"
           gridRow="span 2"
           backgroundColor={colors.primary[400]}
@@ -238,8 +259,8 @@ const Dashboard = () => {
           ))}
         </Box> */}
 
-        {/* ROW 3 */}
-        {/* <Box
+          {/* ROW 3 */}
+          {/* <Box
           gridColumn="span 4"
           gridRow="span 2"
           backgroundColor={colors.primary[400]}
@@ -298,9 +319,10 @@ const Dashboard = () => {
             <GeographyChart isDashboard={true} />
           </Box>
         </Box> */}
+        </Box>
       </Box>
-    </Box>
-  ));
+    )
+  );
 };
 
 export default Dashboard;
