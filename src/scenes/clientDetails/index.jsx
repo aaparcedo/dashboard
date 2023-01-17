b2830c0fb75a90b23f46f0106c6e28292ac14bc0
Josue7211 <bobbyparzero@gmail.com>
2023-01-17 10:22:11 -0500

    data pull firebase

--- src/scenes/clientDetails/index.jsx
+++ src/scenes/clientDetails/index.jsx
@@ -72,38 +72,46 @@ const ClientDetails = () => {
            <table className="black-text">
             <thead>
               <tr>
-                <th className="basic-details-header">Personal</th>
+                <th className="personal-header">Personal</th>
                 <th></th>
               </tr>
             </thead>
             <tbody className="black-text">
               <tr>
                 <td>Name</td>
+                <td className="client-data">{client.firstName}</td> 
               </tr>
               <tr>
                 <td>Date of Birth</td>
+                <td className="client-data">{client.dateOfBirth}</td>
               </tr>
               <tr>
                 <td>Religion</td>
+                <td className="client-data">{client.religion}</td>
               </tr>
               <tr>
                 <td>Civil Status</td>
+                <td className="client-data">{client.civilStatus}</td>
               </tr>
               <tr>
                 <td>Weight</td>
+                <td className="client-data">{client.weight}</td>
               </tr>
               <tr>
                 <td>Height</td>
+                <td className="client-data">{client.height}</td>
               </tr>
               <tr>
                 <td>Eye Color</td>
+                <td className="client-data">{client.eyeColor}</td>
               </tr>
               <tr>
                 <td>Hair Color</td>
+                <td className="client-data">{client.hairColor }</td>
               </tr>
             </tbody>
           </table>
-          <table className="black-text">
+          <table className="black-text client-table">
             <thead>
               <tr>
                 <th className="basic-details-header">Contact</th>
@@ -113,24 +121,31 @@ const ClientDetails = () => {
             <tbody className="black-text">
               <tr>
                 <td>Email</td>
+                <td className="client-data">{client.email}</td>
               </tr>
               <tr>
                 <td>Phone Number</td>
+                <td className="client-data">{client.phone}</td>
               </tr>
               <tr>
                 <td>Address</td>
+                <td className="client-data">{client.streetAddress}</td>
               </tr>
               <tr>
                 <td>City</td>
+                <td className="client-data">{client.city}</td>
               </tr>
               <tr>
                 <td>Zip Code</td>
+                <td className="client-data">{client.zipCode}</td>
               </tr>
               <tr>
                 <td>State/Region</td>
+                <td className="client-data">{client.state}</td>
               </tr>
               <tr>
                 <td>Country</td>
+                <td className="client-data">{client.country}</td>
               </tr>
             </tbody>
           </table>
@@ -144,24 +159,31 @@ const ClientDetails = () => {
             <tbody className="black-text">
               <tr>
                 <td>Alien Number</td>
+                <td className="client-data">{client.alienNumber}</td>
               </tr>
               <tr>
                 <td>Visa Number</td>
+                <td className="client-data">{client.visaNumber}</td>
               </tr>
               <tr>
                 <td>I-94</td>
+                <td className="client-data">{client.i94Number}</td>
               </tr>
               <tr>
                 <td>Date of Arrival</td>
+                <td className="client-data">{client.dateOfArrival}</td>
               </tr>
               <tr>
                 <td>Immigration Status</td>
+                <td className="client-data">{client.currentImmigrationStatus}</td>
               </tr>
               <tr>
                 <td>USCIS Login</td>
+                <td className="client-data">{client.uscisLogin}</td>
               </tr>
               <tr>
                 <td>USCIS Password</td>
+                <td className="client-data">{client.uscisPass}</td>
               </tr>
             </tbody>
           </table>
